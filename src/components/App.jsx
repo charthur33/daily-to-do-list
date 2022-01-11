5fe3952e669b5a593b23da14f1faa7010fa5ec53
Chris <c.william.arthur@gmail.com>
2022-01-11 10:46:16 -0800

    complete functionality to add list items, update styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,35 @@
-import React from "react";
-import ReactDOM from "react";
+import React, { useState } from "react";
 import InputArea from "./InputArea.jsx";
+import ListItem from "./ListItem.jsx";
 
+function App() {
+  const [listItems, setListItems] = useState([]);
 
+  function addItem(inputText) {
+    setListItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
 
-function App () {
-    return (
-        <div className="container">
-            <div className="heading">
-                <h1>To-Do List</h1>
-            </div>
-            <InputArea />
-        </div>
-    )
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea onAdd={addItem} />
+      <div>
+          <ul>
+              {listItems.map((item, index) => (
+                  <ListItem 
+                      key={index}
+                      id={index}
+                      text={item}
+                  />
+              ))}
+          </ul>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -1,17 +1,32 @@
+//This component will serve as the input section of the to-do list where the user can
+// enter and add new items to the list.
+
 import React, { useState } from "react";
 
 function InputArea(props) {
-    const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState("");
 
-    return (
-        <div className="form">
-            <input type="text" />
-            <button>
-                <span>Add</span>
-            </button>
-        </div>
-    );
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setInputText(newValue);
+    console.log(inputText);
+  }
+
+  return (
+    <div className="form">
+      <input onChange={handleChange} type="text" value={inputText} />
+      <button
+        className="add-button"
+        onClick={() => {
+          props.onAdd(inputText);
+          //After user clicks add, input will be emptied
+          setInputText("");
+        }}
+      >
+        <span>Add</span>
+      </button>
+    </div>
+  );
 }
 
 export default InputArea;
-
