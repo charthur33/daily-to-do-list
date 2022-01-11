b75cda87143a152f76bebd7f797b55c5d692372d
Chris <c.william.arthur@gmail.com>
2022-01-10 17:07:48 -0800

    Create component tree, update styling of notepad

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react";
+
+
+
+function App () {
+    return (
+        <div className="container">
+            <div className="heading">
+                <h1>To-Do List</h1>
+            </div>
+        </div>
+    )
+}
+
+export default App;
