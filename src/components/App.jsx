fd5988530cc65311f2dffb9a35ba2522c4621e74
Chris <c.william.arthur@gmail.com>
2022-01-12 16:41:02 -0800

    add today's date, update header styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import ListItem from "./ListItem.jsx";
 
 function App() {
   const [listItems, setListItems] = useState([]);
+  const currentDate = new Date();
+  const dateString = currentDate.toDateString();
 
   function addItem(inputText) {
     setListItems((prevItems) => {
@@ -15,6 +17,9 @@ function App() {
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
+      </div>
+      <div className="heading">
+        <h2>{dateString}</h2>
       </div>
       <InputArea onAdd={addItem} />
       <div>
